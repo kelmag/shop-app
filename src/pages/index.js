ad45b716812f303d2878c65f1412d41560de4196
kelmag <khaliilelmaghraoui@gmail.com>
2019-08-06 01:39:06 +0100

    Initialized Landing page with empty sections

---
+++ src/pages/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Layout from '../templates/landing';
+import {
+    AboutSection,
+    ArticleSection,
+    FeaturedSection,
+    QuoteSection,
+    ShopSection,
+    SocialMediaSection,
+    SpecificShopSection,
+    SubscribeSection,
+    Swipper,
+} from '../components/pages/landing'
+
+const App = () => {
+    return (
+        <Layout>
+            <Swipper />
+            <AboutSection />
+            <FeaturedSection />
+            <ArticleSection />
+            <ShopSection />
+            <QuoteSection />
+            <SocialMediaSection />
+            <SpecificShopSection />
+            <SubscribeSection />
+        </Layout>
+    );
+};
+
+export default App;
