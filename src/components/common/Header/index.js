1e494a196f5a52dfa87e0874b7b1933d8e457346
kelmag <khaliilelmaghraoui@gmail.com>
2019-08-06 01:38:22 +0100

    Added Header Basic Components

--- src/components/common/Header/index.js
+++ src/components/common/Header/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import * as styles from './styles'
+import { Icon } from '../../kit'
 
 const Header = () => {
     return (
         <styles.Wrap >
-            Header
+            <styles.Search >
+                <Icon value="fa-spin fa-spinner" />
+            </styles.Search>
+            <styles.NavBar ></styles.NavBar>
+            <styles.Title ></styles.Title>
+            <styles.Profile ></styles.Profile>
+            <styles.Favorite ></styles.Favorite>
+            <styles.Cart ></styles.Cart>
         </styles.Wrap>
     );
 };
--- src/components/common/Header/styles.js
+++ src/components/common/Header/styles.js
@@ -1,3 +1,9 @@
 import styled from 'styled-components'
 
 export const Wrap = styled.div``;
+export const Search = styled.div``;
+export const NavBar = styled.div``;
+export const Title = styled.div``;
+export const Profile = styled.div``;
+export const Favorite = styled.div``;
+export const Cart = styled.div``;
