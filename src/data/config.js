f909c2ceb4fa53781baa354ffefe87636a2b6a7d
kelmag <khaliilelmaghraoui@gmail.com>
2019-08-08 20:02:38 +0100

    Add constants and config file

---
+++ src/data/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    title: 'Bicycle Company',
+    defaultTitle: 'Bicycle Company — The Best Bicycle Company Brand',
+    logo: 'https://www.bicyclecompany.com/favicon/favicon-512.png',
+    url: 'https://www.bicyclecompany.com',
+    defaultCover: 'https://www.bicyclecompany.com/bicyclecompany-icon.png',
+    legalName: 'bicyclecompany',
+    defaultDescription:
+        'A platform to shop your bike accessories and discover all about bike products.',
+    socialLinks: {
+        twitter: 'https://twitter.com/bicyclecompany',
+        github: 'https://github.com/bicyclecompany',
+        linkedin: 'https://www.linkedin.com/company/bicyclecompany',
+        instagram: 'https://www.instagram.com/bicyclecompany/',
+        youtube: 'https://www.youtube.com/bicyclecompany',
+    },
+    googleAnalyticsID: 'UA-xxxxx-x',
+    themeColor: '#000',
+    backgroundColor: '#000',
+    social: {
+        facebook: 'xxxx',
+        twitter: '@bicyclecompany',
+    },
+    defaultAuthor: 'default author',
+    siteRss: '/rss.xml',
+    google: 'xxxx',
+}
