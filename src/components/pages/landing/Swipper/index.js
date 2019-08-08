01a0aa83bdebe381fb003cc8665d1aaec9f7eb67
kelmag <khaliilelmaghraoui@gmail.com>
2019-08-08 21:37:09 +0100

    Added Swipper Basic UI

--- src/components/pages/landing/Swipper/index.js
+++ src/components/pages/landing/Swipper/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import * as styles from './styles'
+import swipper1 from 'images/swipper/swipper1.png';
 
 const SwipperSection = () => {
     return (
-        <div>
-            SwipperSection
-        </div>
+        <styles.Wrap>
+            <styles.Img src={swipper1} alt="" />
+            <styles.LeftButton left icon="fa-angle-left" />
+            <styles.RightButton right icon="fa-angle-right" />
+        </styles.Wrap>
     );
 };
 
--- src/components/pages/landing/Swipper/styles.js
+++ src/components/pages/landing/Swipper/styles.js
@@ -1,3 +1,19 @@
 import styled from 'styled-components'
+import { Rounded } from 'components/kit/Button'
 
-export const Wrap = styled.div``;
+
+export const Wrap = styled.div`
+    position:relative;
+    background:black;
+`;
+
+export const LeftButton = styled(Rounded)``;
+
+export const RightButton = styled(Rounded)``;
+
+export const Img = styled.img`
+    width:100%;
+    height: auto;
+    opacity: 0.5;
+
+`;
