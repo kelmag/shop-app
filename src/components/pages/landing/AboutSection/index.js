72a906c6d4bcfff42a19cbec5809845f3d02efff
kelmag <khaliilelmaghraoui@gmail.com>
2019-08-11 20:42:43 +0100

    Added AboutSection component

--- src/components/pages/landing/AboutSection/index.js
+++ src/components/pages/landing/AboutSection/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import * as styles from './styles'
+import product1 from 'images/about/product1.jpg';
+import product2 from 'images/about/product2.png';
+import ImgContainer from './components/ImgContainer'
 
-const AboutSection = () => {
+const AboutSection = ({ title, description, button }) => {
     return (
-        <div>
-            AboutSection
-        </div>
+        <styles.Wrap>
+            <styles.Content>
+                <styles.Title>{title}</styles.Title>
+                <styles.Description>{description}</styles.Description>
+                <styles.Button value={button} />
+            </styles.Content>
+            <ImgContainer images={[product2, product1]} />
+        </styles.Wrap>
     );
 };
 
--- src/components/pages/landing/AboutSection/styles.js
+++ src/components/pages/landing/AboutSection/styles.js
@@ -1,3 +1,40 @@
 import styled from 'styled-components'
+import { Pill } from 'components/kit/Button'
+import { Wrapper } from 'components/common';
 
-export const Wrap = styled.div``;
+export const Wrap = styled(Wrapper)`
+    flex-direction:row;
+    @media (max-width: ${({ theme }) => theme.screens.medium}) {
+        flex-direction:column;
+    }
+`;
+
+export const Title = styled.h2`
+    text-transform:uppercase;
+    font-size: ${({ theme }) => theme.fonts.larger};
+    font-weight: 800;
+    @media (max-width: ${({ theme }) => theme.screens.tiny}) {
+        font-size: 30pt;
+    }
+`;
+export const Description = styled.p`
+    font-size: 18pt;
+    text-align: justify;
+    color: ${({ theme }) => theme.colors.lightBlack};
+    letter-spacing: .8px;
+    margin-bottom:4rem;
+    @media (max-width: ${({ theme }) => theme.screens.tiny}) {
+    }
+`;
+export const Content = styled.div`
+    flex:6;
+    margin-right: 80px;
+    @media (max-width: ${({ theme }) => theme.screens.big}) {
+        margin-right: 0;
+    }
+`;
+
+export const Button = styled(Pill)`
+        padding: 13px 50px;
+        color:#bbbbbb;
+`;
