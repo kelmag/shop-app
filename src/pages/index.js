5f75d16617a105df0aaad7f8b4bdf3341205d717
kelmag <khaliilelmaghraoui@gmail.com>
2019-08-11 20:45:25 +0100

    Added title and description for AboutSection

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from '../templates/landing';
+import Layout from 'templates/landing';
 import {
     AboutSection,
     ArticleSection,
@@ -10,7 +10,7 @@ import {
     SpecificShopSection,
     SubscribeSection,
     Swipper,
-} from '../components/pages/landing'
+} from 'components/pages/landing'
 
 const Landing = () => {
     return (
@@ -18,9 +18,12 @@ const Landing = () => {
             <Swipper
                 title="Awesome bicycle products"
                 button="More info"
-                right
             />
-            <AboutSection />
+            <AboutSection
+                title="Who we are"
+                description="We are one of the best bicycle companies in the world, We provide the best quality products of bicycle and no one can be compared to our service because we are the best and we will be, we deliver all products with brand new quality and services. Any product is available in different colors and they are very good looking, we take very less delivery charge in comparison to other companies"
+                button="More info"
+            />
             <FeaturedSection />
             <ArticleSection />
             <ShopSection />
