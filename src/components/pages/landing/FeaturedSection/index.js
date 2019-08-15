3b58e597365455db0d84e03efc517fd43cd7932a
kelmag <khaliilelmaghraoui@gmail.com>
2019-08-15 16:26:16 +0100

    Added Basic FeaturedSection component

--- src/components/pages/landing/FeaturedSection/index.js
+++ src/components/pages/landing/FeaturedSection/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import * as styles from './styles'
+import Card from './Card'
 
-const FeaturedSection = () => {
+const FeaturedSection = ({ title, products: { products } }) => {
     return (
         <div>
-            FeaturedSection
+            {title}
+            <styles.CardWrap direction='row'>
+                <styles.CardWrap direction='column' flex="7" >
+                    <Card className="rectangle" product={products[0]} color="lightGray" />
+                    <styles.CardWrap direction='row'>
+                        <Card className="square" flex="1" product={products[2]} color="darkRed" />
+                        <Card className="square" flex="1" product={products[3]} color="gold" />
+                    </styles.CardWrap>
+                </styles.CardWrap>
+                <Card className="portrait" flex="3" product={products[1]} color="darkerGray" />
+
+            </styles.CardWrap>
         </div>
     );
 };
--- src/components/pages/landing/FeaturedSection/styles.js
+++ src/components/pages/landing/FeaturedSection/styles.js
@@ -1,3 +1,21 @@
 import styled from 'styled-components'
 
+export const CardWrap = styled.div`
+    ${({ direction }) => direction === "column" ? 'flex-direction:column;' : 'flex-direction:row;'};
+    ${({ flex }) => flex && `flex:${flex};`};
+    display:flex;
+    @media (max-width: ${({ theme }) => theme.screens.medium}) {
+        flex-direction:column;
+        & div .square-img {
+            display:block;
+            img {
+                max-height: 140px;
+                width: auto;
+            }
+        }
+        & .normal-img, .portrait div.normal-img{
+            display:none;
+        }
+    }
+`;
 export const Wrap = styled.div``;
