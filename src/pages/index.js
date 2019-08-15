f7f26551960610cbbe3ea4705e51375f802c7bce
kelmag <khaliilelmaghraoui@gmail.com>
2019-08-15 16:28:18 +0100

    Passing data into FeaturedSection

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,7 @@ import {
     SubscribeSection,
     Swipper,
 } from 'components/pages/landing'
+import products from 'data/products'
 
 const Landing = () => {
     return (
@@ -24,7 +25,10 @@ const Landing = () => {
                 description="We are one of the best bicycle companies in the world, We provide the best quality products of bicycle and no one can be compared to our service because we are the best and we will be, we deliver all products with brand new quality and services. Any product is available in different colors and they are very good looking, we take very less delivery charge in comparison to other companies"
                 button="More info"
             />
-            <FeaturedSection />
+            <FeaturedSection
+                title="Featured products"
+                products={products}
+            />
             <ArticleSection />
             <ShopSection />
             <QuoteSection />
