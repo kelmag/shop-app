52af3a1135b552d28b71c3211b72ddaaeaa8b2fe
kelmag <khaliilelmaghraoui@gmail.com>
2019-08-15 16:27:45 +0100

    Added products Data & images

---
+++ src/data/products.js
@@ -0,0 +1,30 @@
+import * as products from 'images/products';
+
+export default {
+    products: [
+        {
+            category: 'Mountain Bike',
+            name: 'Carbon Rims Wide',
+            price: '700',
+            image: products.mountainBike,
+        },
+        {
+            category: 'Aero System',
+            name: 'Redshift Dual-post seatpost',
+            price: '700',
+            image: products.redshift,
+        },
+        {
+            category: 'Frames',
+            name: 'Zealous Div Frame',
+            price: '900',
+            image: products.zealous,
+        },
+        {
+            category: 'Aero system',
+            name: 'ShockStop suspension stem',
+            price: '700',
+            image: products.shockstop,
+        },
+    ],
+}
---
+++ src/images/products/index.js
@@ -0,0 +1,4 @@
+export { default as mountainBike } from './mountainBike.png'
+export { default as redshift } from './redshift.png'
+export { default as zealous } from './zealous.png'
+export { default as shockstop } from './shockstop.png'
