3252958a924ad26160262a8adca5a5de70017e53
kelmag <khaliilelmaghraoui@gmail.com>
2019-08-22 14:58:30 +0100

    Added footer component

--- src/components/common/Footer/index.js
+++ src/components/common/Footer/index.js
@@ -1,10 +1,67 @@
 import React from 'react';
 import * as styles from './styles'
-const Footer = () => {
+import { Icon } from 'components/kit'
+const Footer = ({ title }) => {
+    let myGithubLink = "https://github.com/kelmag";
     return (
-        <div>
-            Footer
-        </div>
+        <styles.Wrap>
+            <styles.Info>
+                <styles.Brand>
+                    {title}
+                </styles.Brand>
+                <p>
+                    We are one of the best bicycle companies in the world, We provide the best quality products of bicycle and no one can be compared to our service
+                </p>
+                <styles.Actions >
+                    <a href={myGithubLink}>Guides</a>
+                    <a href={myGithubLink}>Terms of Use</a>
+                    <a href={myGithubLink}>Privacy Policy</a>
+                </styles.Actions>
+                <p>Copyright 2019 <a href={myGithubLink}> @kelmag</a> Bicycle company</p>
+            </styles.Info>
+            <styles.Links>
+                <styles.Wrapper>
+                    <styles.Title>
+                        Quick links
+                    </styles.Title>
+                    <styles.Actions direction='column'>
+                        <a href={myGithubLink}>Search</a>
+                        <a href={myGithubLink}>Shipping and Delivery</a>
+                        <a href={myGithubLink}>Payment Options</a>
+                        <a href={myGithubLink}>Contact Us</a>
+                    </styles.Actions>
+                </styles.Wrapper>
+                <styles.Wrapper>
+                    <styles.Title>
+                        About us
+                    </styles.Title>
+                    <styles.Actions direction='column'>
+                        <a href={myGithubLink}>Our Story</a>
+                        <a href={myGithubLink}>Maintenance</a>
+                        <a href={myGithubLink}>Service</a>
+                        <a href={myGithubLink}>Products</a>
+                    </styles.Actions>
+                </styles.Wrapper>
+                <styles.SocialMedia>
+                    <styles.Title>
+                        Follow us
+                </styles.Title>
+                    <styles.Actions place="center" color="light" >
+                        <a href={myGithubLink}><Icon value="fa-facebook fa-2x" /></a>
+                        <a href={myGithubLink}><Icon value="fa-twitter fa-2x" /></a>
+                        <a href={myGithubLink}><Icon value="fa-instagram fa-2x" /></a>
+                    </styles.Actions>
+
+                    <p>
+                        Payments method
+                </p>
+                    <styles.Actions place="center" color="light" >
+                        <a href={myGithubLink}><Icon value="fa-cc-paypal fa-2x" /></a>
+                        <a href={myGithubLink}><Icon value="fa-cc-visa fa-2x" /></a>
+                    </styles.Actions>
+                </styles.SocialMedia>
+            </styles.Links>
+        </styles.Wrap>
     );
 };
 
--- src/components/common/Footer/styles.js
+++ src/components/common/Footer/styles.js
@@ -1,3 +1,106 @@
 import styled from 'styled-components'
+import { Wrapper as OriginalWrapper } from 'components/common';
 
-export const Wrap = styled.div``;
+export const StackView = styled.div`
+    @media (max-width: ${({ theme }) => theme.screens.medium}) {
+        margin-top:1rem;
+    }
+`;
+
+export const Info = styled(StackView)`
+    display:flex;
+    flex-direction:column;
+    flex:4;
+    padding:0 40px 0 0;
+    color:${({ theme }) => theme.colors.darkGray};
+    p{
+        text-align:justify;
+        a {
+            color:${({ theme }) => theme.colors.red};
+        }
+    }
+    @media (max-width: ${({ theme }) => theme.screens.small}) {
+        padding:0 ;
+    }
+`;
+
+export const Links = styled(StackView)`
+    display:flex;
+    flex-direction:row;
+    flex:4;
+    @media (max-width: ${({ theme }) => theme.screens.tiny}) {
+        flex-direction:column;
+    }
+`;
+
+export const Wrapper = styled.div`
+    display:flex;
+    flex-direction:column;
+    flex:1;
+    @media (max-width: ${({ theme }) => theme.screens.tiny}) {
+        text-align: center;
+        margin-top:1rem;
+    }
+`;
+
+export const Actions = styled.div`
+    ${({ direction }) => direction === "column" ? 'flex-direction:column;' : 'flex-direction:row;'};
+    ${({ place }) => place ? `place-content:${place};` : 'place-content:flex-start;'};
+    & > a {
+        ${({ color, theme }) => color ?
+        (theme.colors[color] ? `color:${theme.colors[color]};` : `color:${color};`)
+        : `color:${theme.colors.lightGray};`};
+    }
+    display:flex;
+    flex:1;
+    @media (max-width: ${({ theme }) => theme.screens.smaller}) {
+        place-content:space-between;
+    }
+`;
+
+export const SocialMedia = styled.div`
+    display:flex;
+    flex-direction:column;
+    flex:2;
+    text-align:center;
+    color:${({ theme }) => theme.colors.darkGray};
+    @media (max-width: ${({ theme }) => theme.screens.tiny}) {
+        margin-top:2rem;
+    }
+`;
+
+export const Wrap = styled(OriginalWrapper)`
+    display:flex;
+    flex-direction:row;
+    background-color:${({ theme }) => theme.colors.lighterBlack};
+    color:${({ theme }) => theme.colors.light};
+    a{
+        text-decoration:none;
+        margin:5px;
+    }
+    @media (max-width: ${({ theme }) => theme.screens.medium}) {
+        flex-direction:column;
+    }
+`;
+
+export const Brand = styled.div`
+    flex : 5;
+    text-transform: uppercase;
+    font-family: cursive;
+    color:${({ theme }) => theme.colors.light};
+    font-size: ${({ theme }) => theme.fonts.large};
+    line-height: 0.7;
+    margin-bottom:2rem;
+    @media (max-width: ${({ theme }) => theme.screens.tiny}) {
+        font-size: 22pt;
+    }
+`;
+
+export const Title = styled.div`
+    font-size: ${({ theme }) => theme.fonts.medium};
+    color:${({ theme }) => theme.colors.light};
+    margin-bottom:3rem;
+    @media (max-width: ${({ theme }) => theme.screens.tiny}) {
+        margin-bottom:1rem;
+    }
+`;
