ce207f3eade125f3dc31df85a977bf19e18ff00c
kelmag <khaliilelmaghraoui@gmail.com>
2019-08-23 02:05:35 +0100

    hot fixes and added Modal closing handler

--- src/components/kit/Button/Pill/index.js
+++ src/components/kit/Button/Pill/index.js
@@ -2,13 +2,16 @@ import React, { useContext } from 'react';
 import * as styles from './styles'
 import { ModalContext } from 'context/ModalContext'
 
-const Pill = ({ icon, value, right, left, className, ...props }) => {
+
+
+const Pill = ({ icon, value, right, left, className, onClick, ...props }) => {
     const { setDisplayModal } = useContext(ModalContext);
+    const handleClick = () => setDisplayModal(true)
     return (
-        <styles.Wrap right={right} left={left} className={className} {...props} onClick={() => { setDisplayModal(true) }}>
+        <styles.Wrap right={right} left={left} className={className} onClick={onClick || handleClick} {...props} >
             <i className={"fa " + icon}></i>
             {value}
-        </styles.Wrap>)
+        </styles.Wrap >)
 
 };
 
